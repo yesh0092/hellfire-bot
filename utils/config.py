974666d162ce11c4afda2aaa0f94c879bbacb370
yesh0092 <yesh00941@gmail.com>
2026-01-30 11:57:31 +0530

    Update config.py

---
+++ utils/config.py
@@ -0,0 +1,13 @@
+STAFF_ROLES = [
+    "Staff",
+    "Staff+",
+    "Staff++",
+    "Staff+++"
+]
+
+SUPPORT_CATEGORY_NAME = "SUPPORT"
+
+COLOR_PRIMARY = 0x020617
+COLOR_SECONDARY = 0x1f2937
+COLOR_DANGER = 0x7c2d12
+COLOR_GOLD = 0xD4AF37
